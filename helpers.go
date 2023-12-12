bf458e2a70b578115a132e087b67bbb491bfab16
Robert Marrett <Robmmar90@gmail.com>
2023-12-12 10:10:36 -0700

    update

--- helpers.go
+++ helpers.go
@@ -1,3 +1,19 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/ItsJustVaal/HoloGo/internal/models"
+)
+
+func handleGetReadiness(w http.ResponseWriter, r *http.Request) {
+	respondWithJSON(w, http.StatusOK, models.JsonResponse{
+		Status: "ok",
+	})
+}
+
+func handleGetErr(w http.ResponseWriter, r *http.Request) {
+	respondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+}
+
 func addChannelsToDB() {}
